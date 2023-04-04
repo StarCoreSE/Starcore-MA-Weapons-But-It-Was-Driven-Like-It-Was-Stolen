7fac1b391be5c2d8d8a01dd7ff2e2d398a608de4
BadM0dder <npool@emich.edu>
2023-04-04 19:03:17 -0400

    set barrels back, from 2 to 1 per shot on slinger whoops

--- Data/Scripts/CoreParts/MA_150_Part.cs
+++ Data/Scripts/CoreParts/MA_150_Part.cs
@@ -159,8 +159,8 @@ namespace Scripts {
                 },
                 Loading = new LoadingDef
                 {
-                    RateOfFire = 60, // Set this to 3600 for beam weapons.
-                    BarrelsPerShot = 2, // How many muzzles will fire a projectile per fire event.
+                    RateOfFire = 100, // Set this to 3600 for beam weapons.
+                    BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
                     ReloadTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
